343017430b69637c06f1d5d3e84cb346b3a79a00
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-06-22 23:19:20 +0530

    fix::> added mutex.go

---
+++ test/mutex.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+type safeCounter struct {
+	counts map[string]int
+	mux    *sync.Mutex
+}
+
+func (sc safeCounter) inc(key string) {
+	sc.mux.Lock()
+	defer sc.mux.Unlock()
+	sc.slowIncrement(key)
+}
+
+func (sc safeCounter) val(key string) int {
+	sc.mux.Lock()
+	defer sc.mux.Unlock()
+	return sc.counts[key]
+}
+
+func (sc safeCounter) slowIncrement(key string) {
+	tempCounter := sc.counts[key]
+	time.Sleep(time.Microsecond * 10)
+	tempCounter++
+	sc.counts[key] = tempCounter
+}
+
+type emailTest struct {
+	email string
+	count int
+}
