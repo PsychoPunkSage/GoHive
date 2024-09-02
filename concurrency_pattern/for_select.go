a73bed50770ae71a6cfd347e3746ede01ca094ad
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-09-02 07:22:22 +0530

    fix::> for_select style primitive done

---
+++ concurrency_pattern/for_select.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// func main() {
+// 	// for_select()
+// 	infGoRoutine()
+// }
+
+func for_select() {
+	forChannel := make(chan string, 7) // buffered channel
+	chars := []string{"a", "b", "c", "d", "e", "f", "g"}
+	for _, char := range chars {
+		select {
+		case forChannel <- char:
+		}
+	}
+
+	close(forChannel)
+
+	for ch := range forChannel {
+		fmt.Println(ch)
+	}
+}
+
+func infGoRoutine() {
+	go func() {
+		for {
+			select {
+			default:
+				fmt.Println("Hello")
+			}
+		}
+	}()
+
+	time.Sleep(5 * time.Second)
+}
