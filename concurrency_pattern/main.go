9d546bbd5e7527ca8798017386d579e7461f61ed
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-09-02 07:22:42 +0530

    fix::> stopped main.go

--- concurrency_pattern/main.go
+++ concurrency_pattern/main.go
@@ -1,34 +1,33 @@
 package main
 
-import (
-	"fmt"
-)
+// func main() {
+// 	mychannel := make(chan string)
+// 	anotherChannel := make(chan string)
 
-func main() {
-	mychannel := make(chan string)
-	anotherChannel := make(chan string)
+// 	go func() {
+// 		mychannel <- someFunc("12")
+// 	}()
 
-	go func() {
-		mychannel <- someFunc("12")
-	}()
+// 	go func() {
+// 		anotherChannel <- someFunc("34")
+// 	}()
 
-	go func() {
-		anotherChannel <- someFunc("34")
-	}()
+// 	// msg := <-mychannel
 
-	// msg := <-mychannel
+// 	// fmt.Println("Message:> ", msg)
 
-	// fmt.Println("Message:> ", msg)
+// 	// fmt.Println("Hi")
 
-	// fmt.Println("Hi")
+// 	select { // its gonna block until it receives info from one of the channels.
+// 	case msgMychannel := <-mychannel:
+// 		fmt.Println(msgMychannel)
+// 	case msgAnotherChannel := <-anotherChannel:
+// 		fmt.Println(msgAnotherChannel)
+// 	}
 
-	select { // its gonna block until it receives info from one of the channels.
-	case msgMychannel := <-mychannel:
-		fmt.Println(msgMychannel)
-	case msgAnotherChannel := <-anotherChannel:
-		fmt.Println(msgAnotherChannel)
-	}
-}
+// 	// for-select
+// 	for_select()
+// }
 
 func someFunc(num string) string {
 	return num + "test"
